e7105cdb3b4f17f4382da6996ca0c0569971dcbc
bergaman9 <omerguler53@gmail.com>
2023-09-06 18:35:20 +0300

    README.md update and ENV variables changed

---
+++ README.md
@@ -0,0 +1,8 @@
+## USAGE
+Create an .env file and enter the following information:
+```
+AUTHORIZATION=""
+SESSION_ID=""
+GUILD_ID=""
+CHANNEL_ID=""
+```
--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 headers={"authorization": os.getenv("AUTHORIZATION")}
-turkoyto_payload={"type": 2, "application_id":"302050872383242240","guild_id":os.getenv("TURKOYTO_GID"), "session_id": os.getenv("SESSION_ID"),"channel_id": os.getenv("TURKOYTO_CID"), "data": {"version": "1051151064008769576", "id": "947088344167366698", "name": "bump","type": 1}}
+payload={"type": 2, "application_id":"302050872383242240","guild_id":os.getenv("GUILD_ID"), "session_id": os.getenv("SESSION_ID"),"channel_id": os.getenv("CHANNEL_ID"), "data": {"version": "1051151064008769576", "id": "947088344167366698", "name": "bump","type": 1}}
 
 while True:
-    requests.post("https://discord.com/api/v9/interactions", headers=headers, json=turkoyto_payload)
+    requests.post("https://discord.com/api/v9/interactions", headers=headers, json=payload)
     print("Türk Oyuncu Topluluğu öne çıkarıldı.")
     time.sleep(7200)
